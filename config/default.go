0abb5e661114988029813bd3ef508e1d437233b7
Alan-maqing <maqing@dongyu-technology.com>
2023-08-28 17:12:33 +0800

    增加配置文件

--- config/default.go
+++ config/default.go
@@ -1,11 +1,23 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
 
 type Config struct {
-	DBUri    string `mapstructure:"MONGODB_LOCAL_URI"`
-	RedisUri string `mapstructure:"REDIS_URI"`
-	Port     string `mapstructure:"PORT"`
+	DBUri                  string        `mapstructure:"MONGODB_LOCAL_URI"`
+	RedisUri               string        `mapstructure:"REDIS_URI"`
+	Port                   string        `mapstructure:"PORT"`
+	AccessTokenPrivateKey  string        `mapstructure:"ACCESS_TOKEN_PRIVATE_KEY"`
+	AccessTokenPublicKey   string        `mapstructure:"ACCESS_TOKEN_PUBLIC_KEY"`
+	RefreshTokenPublicKey  string        `mapstructure:"REFRESH_TOKEN_PUBLIC_KEY"`
+	RefreshTokenPrivateKey string        `mapstructure:"REFRESH_TOKEN_PRIVATE_KEY"`
+	AccessTokenExpiresIn   time.Duration `mapstructure:"ACCESS_TOKEN_EXPIRES_IN"`
+	RefreshTokenExpiresIn  time.Duration `mapstructure:"REFRESH_TOKEN_EXPIRES_IN"`
+	AccessTokenMaxAge      int           `mapstructure:"ACCESS_TOKEN_MAX_AGE"`
+	RefreshTokenMaxAge     int           `mapstructure:"REFRESH_TOKEN_MAX_AGE"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
